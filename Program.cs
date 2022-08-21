44b4117e38e66d966467e8a952f29e1c6b6d6b58
Hope Ross <hope.ross@daveramsey.com>
2022-08-20 20:46:44 -0500

    added swagger, I think

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using social_api.Migrations;
 using social_api.Repositories;
 
@@ -11,7 +12,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "social-api", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+        In = ParameterLocation.Header, 
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey 
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    { 
+        new OpenApiSecurityScheme 
+        { 
+        Reference = new OpenApiReference 
+        { 
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer" 
+        } 
+        },
+        new string[] { } 
+        } 
+    });
+});
 
 builder.Services.AddSqlite<SocialDbContext>("Data Source=socialAPI.db");
 builder.Services.AddScoped<IPostRepository, PostRepository>();
